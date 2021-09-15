0a57ccf6438f8c8ead6509bae42faea91c290eab
David Barringer <davidnbarringer@gmail.com>
2021-09-15 14:38:31 -0600

    Hello World!

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String text = "Hello World!";
+        System.out.println(text);
     }
     
 }
